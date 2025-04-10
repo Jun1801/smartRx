a0063731ebb83b4c9cb6a9831f8d3367cb714713
CryAndRRich <willshane2006@gmail.com>
2025-04-10 19:54:12 +0700

    Add files via upload

---
+++ run_web.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from st_pages import add_page_title, get_nav_from_toml
+
+nav = get_nav_from_toml(".streamlit/pages.toml")
+pg = st.navigation(nav)
+add_page_title(pg)
+
+pg.run()

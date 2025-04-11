ac53ffd8eb5472c0699d005abac9f0bbd74e1de3
CryAndRRich <willshane2006@gmail.com>
2025-04-12 02:06:00 +0700

    Update 2am

--- smartrx_web/smartrx.py
+++ smartrx_web/smartrx.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.markdown("### 👨‍⚕️ SmartRx - AI for checking drugs and food interactions ")
-
+st.markdown("Chào mừng bạn đến với SmartRx, công cụ trợ giúp kiểm tra và phát hiện tương tác thuốc - thuốc hoặc thuốc - thực phẩm nhằm giúp bạn giảm đi mối lo ngại khi sử dụng thuốc")
 st.image("smartrx_web/images/smartrx.jpg")
 
 st.info("Original Repository on [Github](https://github.com/CryAndRRich/smartRx)")
@@ -9,7 +9,11 @@ st.info("Original Repository on [Github](https://github.com/CryAndRRich/smartRx)
 st.markdown("---")
 
 st.markdown("### 🌎 Thực trạng hiện nay")
+st.markdown("""
+Bệnh nhân hiện nay mắc nhiều bệnh và phải gặp bác sĩ nhiều lần, chính vì thế, đôi khi họ sẽ không thể nhớ hết thuốc nào dùng khi nào, và liệu có tương tác xấu khi dùng nhiều loại thuốc cho các loại bệnh khác nhau hay không.
 
+Đồng thời, bệnh nhân có nhiều mối quan tâm cũng như nhiều chế độ ăn uống sinh hoạt thì cũng khó mà tuân theo nghiêm ngặt khuyến nghị của bác sĩ, dẫn đến rủi ro lớn về các tương tác thuốc - thực phẩm bất lợi mà họ gặp phải.
+           """)
 with st.expander("Thế giới"):
     st.markdown(
         """
@@ -22,15 +26,15 @@ with st.expander("Thế giới"):
 with st.expander("Việt Nam"):
     st.markdown(
         """
-        Một [bài nghiên cứu](https://jppres.com/jppres/outpatient-prescription-drug-interactions-in-vietnam/) của một nhóm sinh viên Cần Thơ tại một khoa ngoại trú của một trung tâm y tế liên kết với thành phố từ tháng 1 đến tháng 6 năm 2023 chỉ ra rằng có đến **36,7%** đơn thuốc chứa **tương tác thuốc bất lợi**.
+        Một [bài nghiên cứu](https://jppres.com/jppres/outpatient-prescription-drug-interactions-in-vietnam/) của một nhóm sinh viên Cần Thơ tại một khoa ngoại trú của một trung tâm y tế liên kết với thành phố từ tháng 1 đến tháng 6 năm 2023 chỉ ra rằng có đến **36,7%** đơn thuốc tức là hơn 1/3 đơn được kê chứa **tương tác thuốc bất lợi**.
         """)
     
 st.markdown("""
 ### 🎯 Mục tiêu
-**SmartRx** là một web app, giúp kiểm tra tương tác thuốc bằng cách đọc ảnh mã vạch của thuốc và hiển thị trang chi tiết thuốc, bao gồm thông tin mô tả, tác dụng phụ, cũng như các tương tác giữa thuốc này và các loại thuốc có trong danh sách của người dùng. Chức năng kiểm tra tương tác với thực phẩm cho phép người dùng nhập tên hoặc quét hình ảnh các loại thực phẩm và sau đó cảnh báo lại nếu có bất kỳ tương tác nào với các loại thuốc trong danh sách. 
-""")
+**SmartRx** sẽ hỗ trợ lưu lại hết lịch sử dùng thuốc và bệnh tương ứng cho bệnh nhân, từ đó sẽ phát hiện ra các tương tác thuốc bất lợi. Ứng dụng này đồng thời sẽ tổng hợp và đưa ra những gợi ý cho cả bệnh nhân, nhắc nhở bệnh nhân uống thuốc. Kiểm tra tương tác thuốc - thuốc hay thuốc - thực phẩm một cách đơn giản, tiện lợi.
 
-st.image("smartrx_web/images/usecase.png")
+SmartRx đóng vai trò như một sổ khám bệnh điện tử cho bệnh nhân và mọi đối tượng - thao tác dễ dàng, hiệu quả. 
+""")
 
 hide_streamlit_style = """
 <style>
